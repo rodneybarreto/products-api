41ae82a6587a293ec437e79c6f6080d79a7dc4cb
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-19 21:57:41 -0300

    Rename docker image

--- build.docker-image.sh
+++ build.docker-image.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-# Generate docker image
-docker build . -t rodneybarreto/minipets-products:1.0.0
+## Generate docker image
+docker build . -t rodneybarreto/minipets_products-api:1.0.0
+
+## Run container
+# docker run --name products-api -p 8081:8081 rodneybarreto/minipets_products-api:1.0.0
