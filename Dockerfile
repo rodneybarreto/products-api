da59d8de40920e531c794fa564fe9ab06cc4b3b6
Rodney Barreto <rodney.barreto@pge.ce.gov.br>
2021-03-30 18:46:28 -0300

    Send products images to base64

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ MAINTAINER rodneybarreto
 WORKDIR /opt/minipets/products
 ARG APP=build/libs/products-1.0.0.jar
 COPY ${APP} products.jar
+RUN mkdir /opt/minipets/products/img
+COPY products-images/* /opt/minipets/products/img/
 EXPOSE 8081
 ENTRYPOINT ["java","-jar","products.jar"]
--- src/main/kotlin/br/dev/rodneybarreto/minipets/products/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/br/dev/rodneybarreto/minipets/products/service/impl/ProductServiceImpl.kt
@@ -3,8 +3,13 @@ package br.dev.rodneybarreto.minipets.products.service.impl
 import br.dev.rodneybarreto.minipets.products.domain.dto.Product
 import br.dev.rodneybarreto.minipets.products.domain.repository.ProductRepository
 import br.dev.rodneybarreto.minipets.products.service.ProductService
+import org.apache.tomcat.util.codec.binary.Base64
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.util.ResourceUtils
+import java.io.File
+import java.nio.file.Files
 
 @Service
 class ProductServiceImpl(
@@ -12,13 +17,28 @@ class ProductServiceImpl(
     val productRepository: ProductRepository
 ): ProductService {
 
+    @Value("\${spring.profiles.active}")
+    lateinit var activeProfile: String
+
     override fun findAll(): List<Product> {
         val products = mutableListOf<Product>()
         productRepository.findAll().map {
-            val product = Product(it.id, it.name, it.description, it.price, it.photo)
+            val product = Product(it.id, it.name, it.description, it.price, toBase64(it.photo))
             products.add(product)
         }
         return products.toList()
+    }
+
+    private fun toBase64(photo: String): String {
+        if (activeProfile.equals("dev")) {
+            val file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + photo)
+            val bytes = Files.readAllBytes(file.toPath())
+            return Base64.encodeBase64String(bytes)
+        } else {
+            val file = File(photo)
+            val bytes = Files.readAllBytes(file.toPath())
+            return Base64.encodeBase64String(bytes)
+        }
     }
 
 }
